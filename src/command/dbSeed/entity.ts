fe1209550f54d1895870678278371a696d643208
Travis Saylor <travis.saylor@gmail.com>
2023-01-01 09:47:52 -0500

    Add Tv Series seed data

---
+++ src/command/dbSeed/entity.ts
@@ -0,0 +1,24 @@
+import { TvSeries } from '@prisma/client';
+
+export const tvSeriesData: TvSeries[] = [
+  {
+    id: 1,
+    title: 'Avatar: The Last Airbender',
+    description: `Avatar: The Last Airbender, also known as Avatar: The Legend of Aang in some PAL regions, is an Emmy award-winning American animated television series that aired for three seasons on Nickelodeon and the Nicktoons Network. The series was created and produced by Michael Dante DiMartino and Bryan Konietzko, who served as executive producers along with Aaron Ehasz. Avatar is set in an Asian-influenced world of martial arts and elemental manipulation. The show drew on elements from East Asian, South Asian, and Western culture, making it a mixture of what were previously traditionally separate categories of Japanese anime and Western domestic cartoons.
+    The series follows the adventures of the main protagonist Aang and his friends, who must save the world by defeating Fire Lord Ozai and ending the destructive war with the Fire Nation. The show first aired on February 21, 2005 and the series concluded with a widely lauded two-hour television movie on July 19, 2008. The show is now available for purchase on DVD, Blu-ray, the iTunes Store, the Xbox Live Marketplace, the PlayStation Network, Amazon and YouTube. It is also available on streaming platforms such as Netflix, Amazon Prime Video, the Nick website, and Paramount Global owned streaming platform, Paramount+. The show is occasionally aired on Nickelodeon's spinoff network, Nicktoons.
+    
+    Avatar: The Last Airbender was popular with both audiences and critics, garnering 5.6 million viewers on its best-rated showing and receiving high ratings in the Nicktoons lineup, even outside its 6–11-year-old demographic. Avatar has been nominated for and won awards from the Annual Annie Awards, the Genesis Awards, and the primetime Emmy Awards, among others. The first season's success prompted Nickelodeon to order second and third seasons. The first part of a movie trilogy titled The Last Airbender was released on July 1, 2010, and a live-action reimagining produced by Netflix in partnership with Nickelodeon which entered production in 2021.
+    
+    Merchandise based on the series includes scaled action figures, a trading card game, three video games based individually on each season, stuffed animals distributed by Paramount Parks, and two LEGO sets. The series' popularity spawned a sequel series, titled The Legend of Korra, which takes place seventy years after the original series.`,
+    start: 'February 21, 2005',
+    end: 'July 19, 2008',
+  },
+  {
+    id: 2,
+    title: 'The Legend of Korra',
+    description: `The Legend of Korra is an animated American television series set in the Avatar universe as a sequel series to Avatar: The Last Airbender. The premiere episode aired on Nickelodeon on April 14, 2012. However, the first two episodes were released online on March 24-25, 2012 and were available in high-definition for free on iTunes. The series ran for fifty-two episodes, completing its run on December 19, 2014.
+    The series picks up seventy years after the end of the Avatar: The Last Airbender story arc with brand new characters and settings. The protagonist of the new series, Korra, the Avatar after Aang, is a hot-headed and rebellious young woman from the Southern Water Tribe who is "ready to take on the world". The series follows Korra as she faces the difficult challenges, duties, and responsibilities that come with being the Avatar.`,
+    start: 'April 14, 2012',
+    end: 'December 19, 2014',
+  },
+];

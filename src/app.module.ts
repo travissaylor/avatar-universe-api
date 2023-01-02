d0d7eb9d928f7685fc7270aaee14ea1a4b6eca67
Travis Saylor <travis.saylor@gmail.com>
2023-01-02 17:33:02 -0500

    Add read endpoints for seasons

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CharactersModule } from './characters/characters.module';
 import { TvSeriesModule } from './tv-series/tv-series.module';
+import { SeasonsModule } from './seasons/seasons.module';
 
 @Module({
-  imports: [CharactersModule, TvSeriesModule],
+  imports: [CharactersModule, TvSeriesModule, SeasonsModule],
   controllers: [AppController],
   providers: [AppService],
 })
